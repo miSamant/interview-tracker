9a3840803cc2e1301c3b2b717e97ea38453a84af
Mihir Samant <mihirs202@gmail.com>
2026-07-14 15:33:35 -0400

    updating gitignore

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyAfZb8LNHaI8UVvQFNAChwvRxyYrxC8At0",
+  authDomain: "interview-tracker-58159.firebaseapp.com",
+  projectId: "interview-tracker-58159",
+  storageBucket: "interview-tracker-58159.firebasestorage.app",
+  messagingSenderId: "545427084344",
+  appId: "1:545427084344:web:7af340b8b2990a0d5e42cd",
+  measurementId: "G-NRZW4L15QH"
+};
