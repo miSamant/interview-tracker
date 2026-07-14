590cde7c5ca6b6d246600864bc3ae365de2138b0
Mihir Samant <mihirs202@gmail.com>
2026-07-14 12:13:14 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
+import { GoogleAuthProvider, signInWithPopup } from "https://gstatic.com";
+const loginBtn = document.getElementById('login-btn');
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
