00d48debf8c83b2923c2f9a79dc9550d7f2fb3fd
Mihir Samant <mihirs202@gmail.com>
2026-07-14 12:32:32 -0400

    Update firebase-config.js
    Updating config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,3 @@
-
-import { GoogleAuthProvider, signInWithPopup } from "https://gstatic.com";
-const loginBtn = document.getElementById('login-btn');
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAfZb8LNHaI8UVvQFNAChwvRxyYrxC8At0",
@@ -15,7 +8,3 @@ const firebaseConfig = {
   appId: "1:545427084344:web:7af340b8b2990a0d5e42cd",
   measurementId: "G-NRZW4L15QH"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
